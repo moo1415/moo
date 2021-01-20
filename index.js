99db0fd06485d822399a4b4eb524270ae0aa5237
moo <wpxm1415@naver.com>
2021-01-20 19:32:05 +0900

    secret code

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+const config = require("./config/key")
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://moo:asdf1020!@moo.jgcuc.mongodb.net/<dbname>?retryWrites=true&w=majority',{
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+    res.send('안녕하세요 반갑습니다')
 })
 
+
+app.post('/register', (req, res) => {
+
+    //회원가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어둔다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        
+        if (err) return res.json({success: false,err})
+        return res.status(200).json({success: true})
+    })
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
